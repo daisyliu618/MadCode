bffbb058fe66612204bcffe32fe7e24a6677d221
hequanli <762966575@qq.com>
2021-12-13 13:04:09 +0800

    1、示例修改

--- app/src/main/java/com/hequanli/madcode/viewmodel/TestViewModel.kt
+++ app/src/main/java/com/hequanli/madcode/viewmodel/TestViewModel.kt
@@ -1,6 +1,7 @@
 package com.hequanli.madcode.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.hequanli.base.http.mapper.BaseObserver
 import com.hequanli.base.repository.RemoteRepository
 
 /**
@@ -11,10 +12,14 @@ import com.hequanli.base.repository.RemoteRepository
 class TestViewModel : ViewModel() {
 
 
-
     fun getHomeArticleList() {
-        RemoteRepository.me.getHomeArticleList().subscribe {
+        RemoteRepository.me.getHomeArticleList()
+            .subscribe(object : BaseObserver<String>() {
+                override fun succeed(t: String) {
+                }
 
-        }
+                override fun error(msg: String?, code: Int?) {
+                }
+            })
     }
 }
